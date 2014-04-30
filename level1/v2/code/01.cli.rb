f0a5fc992c93f5989b930935fc385f2204e2a915
Sameer Siruguri <siruguri@gmail.com>
2014-04-29 21:00:19 -0700

    made a basic folder for code samples

--- level1/v2/code/01.cli.rb
+++
@@ -1,9 +0,0 @@
-print "This program was run with \n" 
-print ARGV.count.to_s
-print " arguments (ARGV.count).\n"
-
-puts "These arguments are"
-
-ARGV.each_with_index do |arg, index|
-  puts "#{index}: #{arg}"
-end
--- level1/v2/code/02.cmdline_functions.rb
+++
@@ -1,7 +0,0 @@
-def say_hello(name)
-  puts "Hello, #{name}"
-end
-
-ARGV.each do |arg|
-  say_hello(arg)
-end
